018d2718f9b26e5ce678822896b20ff7d74dc818
AlexNfr <nfrcaptain@yandex.ru>
2016-11-03 14:23:25 +0300

    Сделано ДЗ № 1.

---
+++ basic/models/Order.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "order".
+ *
+ * @property integer $id
+ * @property string $name
+ * @property string $address
+ * @property string $email
+ * @property string $phone
+ * @property string $date_create
+ */
+class Order extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'order';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'name', 'address', 'email', 'phone', 'date_create'], 'required'],
+            [['id'], 'integer'],
+            [['date_create'], 'safe'],
+            [['name', 'address', 'email'], 'string', 'max' => 255],
+            [['phone'], 'string', 'max' => 10],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'address' => 'Address',
+            'email' => 'Email',
+            'phone' => 'Phone',
+            'date_create' => 'Date Create',
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     * @return \app\models\query\OrderQuery the active query used by this AR class.
+     */
+    public static function find()
+    {
+        return new \app\models\query\OrderQuery(get_called_class());
+    }
+}
